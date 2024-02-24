f854cdba8c3d3b62f878f3a069775eea36340dcf
anhdao2871989 <anhdao2871989@gmail.com>
2024-02-24 14:58:46 -0600

    firstcommit

---
+++ flavors.js
@@ -0,0 +1,8 @@
+CREATE TABLE flavors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    is_favorite BOOLEAN,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+  
